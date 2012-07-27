e40bf7c5090135174e4b2bcb88cfe1b36cd31bea
Eric Sproul <esproul@omniti.com>
2012-07-27 19:20:42 +0000

    Bump Plack to latest release

--- build/Plack/build.sh
+++ build/Plack/build.sh
@@ -30,7 +30,7 @@
 AUTHORID=MIYAGAWA
 PROG=Plack
 MODNAME=Plack
-VER=0.9989
+VER=1.0001
 VERHUMAN=$VER
 PKG=omniti/perl/$(echo $PROG | tr '[A-Z]' '[a-z]')
 SUMMARY="Perl Superglue for Web frameworks and Web Servers (PSGI toolkit) (Perl $DEPVER)"
