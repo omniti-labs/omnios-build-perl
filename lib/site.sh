e0d28f3ffd647b770cfdd1f30996b6399718a4c4
Eric Sproul <esproul@omniti.com>
2012-12-18 22:26:57 +0000

    Revert to correct default pkg server values

--- lib/site.sh
+++ lib/site.sh
@@ -1,5 +1,3 @@
 # Package server URL and publisher
-#PKGPUBLISHER=omnios-perl
-#PKGSRVR=http://pkg-il-1.int.omniti.net:10008/
-PKGPUBLISHER=root.omnios.omniti.com
-PKGSRVR=http://localhost:888/
+PKGPUBLISHER=omnios-perl
+PKGSRVR=http://pkg-il-1.int.omniti.net:10008/
