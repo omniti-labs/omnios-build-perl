1f32ca8f65a13023d021bde5d9e86c845a2752a3
Eric Sproul <esproul@omniti.com>
2013-11-14 22:35:50 +0000

    update Email::Address, adds new build dep on Capture::Tiny, which now needs to come before Email::Address in the build order

--- build/Email-Address/build.sh
+++ build/Email-Address/build.sh
@@ -30,13 +30,13 @@
 AUTHORID=RJBS
 PROG=Email-Address
 MODNAME=Email::Address
-VER=1.896
+VER=1.900
 VERHUMAN=$VER
 PKG=omniti/perl/$(echo $PROG | tr '[A-Z]' '[a-z]')
 SUMMARY="RFC 2822 Address Parsing (Perl $DEPVER)"
 DESC="$SUMMARY"
 
-BUILD_DEPENDS_IPS="developer/build/gnu-make system/header system/library/math/header-math "
+BUILD_DEPENDS_IPS="developer/build/gnu-make system/header system/library/math/header-math omniti/perl/capture-tiny"
 
 PREFIX=/opt/OMNIperl
 reset_configure_opts
@@ -47,7 +47,7 @@ NO_PARALLEL_MAKE=1
 PERLVERLIST="5.14 5.16"
 
 # Add any additional deps here; omniti/runtime/perl added below
-DEPENDS_IPS=""
+DEPENDS_IPS="omniti/perl/capture-tiny"
 
 # We require a Perl version to use for this build and there is no default
 case $DEPVER in
