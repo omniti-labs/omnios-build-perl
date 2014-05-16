363e8907199d7d626cd334e83cc89d32ac11e939
mmurphy <mmurphy@omniti.com>
2014-05-16 20:58:19 +0000

    tid25388 updating dependencies

--- build/Mouse/build.sh
+++ build/Mouse/build.sh
@@ -36,7 +36,7 @@ PKG=omniti/perl/$(echo $PROG | tr '[A-Z]' '[a-z]')
 SUMMARY="Moose minus the antlers"
 DESC="$SUMMARY"
 
-BUILD_DEPENDS_IPS="developer/build/gnu-make system/header system/library/math/header-math omniti/perl/module-build-xsutil omniti/perl/test-exception"
+BUILD_DEPENDS_IPS="developer/build/gnu-make system/header system/library/math/header-math omniti/perl/module-build-xsutil omniti/perl/test-exception omniti/perl/test-requires"
 
 PREFIX=/opt/OMNIperl
 reset_configure_opts
