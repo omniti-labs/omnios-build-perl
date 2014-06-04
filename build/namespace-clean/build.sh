7e1aed4b8a42f034eae6bde21e4f2a4d63ba8ee7
mmurphy <mmurphy@omniti.com>
2014-06-04 17:27:30 +0000

    tid25579 adding missed dependencies to modules

--- build/namespace-clean/build.sh
+++ build/namespace-clean/build.sh
@@ -36,7 +36,7 @@ PKG=omniti/perl/$(echo $PROG | tr '[A-Z]' '[a-z]')
 SUMMARY="Keep imports and functions out of your namespace"
 DESC="$SUMMARY"
 
-BUILD_DEPENDS_IPS="developer/build/gnu-make system/header system/library/math/header-math omniti/perl/b-hooks-endofscope"
+BUILD_DEPENDS_IPS="developer/build/gnu-make system/header system/library/math/header-math omniti/perl/b-hooks-endofscope omniti/perl/package-stash"
 
 PREFIX=/opt/OMNIperl
 reset_configure_opts
