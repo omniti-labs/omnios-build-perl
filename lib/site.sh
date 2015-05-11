0073c78ccd737c2d22f5f1eb7e4cbcb947f4b7f4
mmurphy <mmurphy@omniti.com>
2015-05-11 16:33:18 +0000

    changing default behavior to publish packages locally so that we dont accidentally publish packages without signing first

--- lib/site.sh
+++ lib/site.sh
@@ -1,11 +1,15 @@
 # Package server URL and publisher
 PKGPUBLISHER=perl.omniti.com
-PKGSRVR=http://pkg-il-1.int.omniti.net:10008/
 
-# Uncommenting this line will create a on-disk repo in
+# This URI is here for information about where we publish
+# our perl modules. You should never publish straight to
+# this repo because we require all packages to be signed.
+#PKGSRVR=http://pkg-il-1.int.omniti.net:10008/
+
+# This line will create a on-disk repo in
 # the top level of your checkout and publish there instead
 # of the URI specified above.
 #
 # The directory does not need to exist ahead of time.
 #
-#PKGSRVR=file:///$MYDIR/../tmp.repo/
+PKGSRVR=file:///$MYDIR/../tmp.repo/
