dd862edf418b8003a2ef8e4022a0c45e37bbbf93
Eric Sproul <esproul@omniti.com>
2016-11-11 18:16:48 +0000

    Bump to latest Data::UUID

--- build/Data-UUID/build.sh
+++ build/Data-UUID/build.sh
@@ -30,7 +30,7 @@
 AUTHORID=RJBS
 PROG=Data-UUID
 MODNAME=Data::UUID
-VER=1.220
+VER=1.221
 VERHUMAN=$VER
 PKG=omniti/perl/$(echo $PROG | tr '[A-Z]' '[a-z]')
 SUMMARY="Perl extension for generating Globally/Universally Unique Identifiers (GUIDs/UUIDs). (Perl $DEPVER)"
