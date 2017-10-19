98154da6d23379065128502394890de0f3d26b83
mmurphy <mmurphy@omniti.com>
2017-10-19 15:20:43 +0000

    GeoIP2 and dependencies

---
+++ build/GeoIP2/build.sh
@@ -0,0 +1,97 @@
+#!/usr/bin/bash
+#
+# CDDL HEADER START
+#
+# The contents of this file are subject to the terms of the
+# Common Development and Distribution License, Version 1.0 only
+# (the "License").  You may not use this file except in compliance
+# with the License.
+#
+# You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
+# or http://www.opensolaris.org/os/licensing.
+# See the License for the specific language governing permissions
+# and limitations under the License.
+#
+# When distributing Covered Code, include this CDDL HEADER in each
+# file and include the License file at usr/src/OPENSOLARIS.LICENSE.
+# If applicable, add the following below this CDDL HEADER, with the
+# fields enclosed by brackets "[]" replaced with your own identifying
+# information: Portions Copyright [yyyy] [name of copyright owner]
+#
+# CDDL HEADER END
+#
+#
+# Copyright 2011-2012 OmniTI Computer Consulting, Inc.  All rights reserved.
+# Use is subject to license terms.
+#
+# Load support functions
+. ../../lib/functions.sh
+
+AUTHORID=MAXMIND
+PROG=GeoIP2
+MODNAME=GeoIP2
+VER=2.003005
+VERHUMAN=$VER
+PKG=omniti/perl/$(echo $PROG | tr '[A-Z]' '[a-z]')
+SUMMARY="Perl API for MaxMind's GeoIP2 web services and databases (Perl $DEPVER)"
+DESC="$SUMMARY"
+
+BUILD_DEPENDS_IPS="developer/build/gnu-make system/header system/library/math omniti/library/geoip2 omniti/perl/data-validate-ip omniti/perl/maxmind-db-reader omniti/perl/moo omniti/perl/sub-exporter-progressive omniti/perl/import-into omniti/perl/namespace-clean omniti/perl/package-stash omniti/perl/b-hooks-endofscope omniti/perl/throwable"
+
+PREFIX=/opt/OMNIperl
+reset_configure_opts
+
+PERL32=/opt/OMNIperl/bin/perl
+LDFLAGS32="$LDFLAGS32 -L/opt/omni/lib -R/opt/omni/lib"
+LDFLAGS64="$LDFLAGS64 -L/opt/omni/lib/$ISAPART64 -R/opt/omni/lib/$ISAPART64"
+LDDLFLAGS32="$LDFLAGS32 -L/opt/omni/lib -R/opt/omni/lib"
+LDDLFLAGS64="$LDFLAGS64 -L/opt/omni/lib/$ISAPART64 -R/opt/omni/lib/$ISAPART64"
+
+NO_PARALLEL_MAKE=1
+PERL_MAKE_TEST="" #broken tests
+
+# Only 5.14 and later will get individual module builds
+PERLVERLIST="5.14 5.16 5.20"
+
+# Add any additional deps here; OMNIperl added below
+DEPENDS_IPS="omniti/library/geoip2 omniti/perl/data-validate-ip omniti/perl/maxmind-db-reader omniti/perl/moo omniti/perl/sub-exporter-progressive omniti/perl/import-into omniti/perl/namespace-clean omniti/perl/package-stash omniti/perl/b-hooks-endofscope omniti/perl/throwable"
+
+# We require a Perl version to use for this build and there is no default
+case $DEPVER in
+    5.14)
+        DEPENDS_IPS="$DEPENDS_IPS omniti/runtime/perl omniti/incorporation/perl-514-incorporation"
+        ;;
+    5.16)
+        DEPENDS_IPS="$DEPENDS_IPS omniti/runtime/perl omniti/incorporation/perl-516-incorporation"
+        ;;
+    5.20)
+        DEPENDS_IPS="$DEPENDS_IPS omniti/runtime/perl omniti/incorporation/perl-520-incorporation"
+        ;;
+    5.26)
+        DEPENDS_IPS="$DEPENDS_IPS omniti/runtime/perl omniti/incorporation/perl-526-incorporation"
+        ;;
+
+    "")
+        logerr "You must specify a version with -d DEPVER. Valid versions: $PERLVERLIST"
+        ;;
+esac
+
+# Uncomment and set PREFIX if any modules install site binaries
+#save_function make_isa_stub make_isa_stub_orig
+#make_isa_stub() {
+#    PREFIX=/usr make_isa_stub_orig
+#}
+
+init
+test_if_core
+download_source CPAN/authors/id/${AUTHORID:0:1}/${AUTHORID:0:2}/${AUTHORID} $PROG $VER
+patch_source
+prep_build
+buildperl
+make_package
+clean_up
+
+unset PERL_MAKE_TEST
+
+# Vim hints
+# vim:ts=4:sw=4:et:
