cc2eaf77652ee6874c3c722ebf2267eddf57d148
mmurphy <mmurphy@omniti.com>
2017-10-30 19:41:16 +0000

    apache-asp updates

--- build/Apache-ASP/build.sh
+++ build/Apache-ASP/build.sh
@@ -36,7 +36,7 @@ PKG=omniti/perl/$(echo $PROG | tr '[A-Z]' '[a-z]')
 SUMMARY="Active Server Pages for Apache with mod_perl (Perl $DEPVER)"
 DESC="$SUMMARY"
 
-BUILD_DEPENDS_IPS="developer/build/gnu-make system/header system/library/math omniti/perl/mldbm omniti/perl/mldbm-sync"
+BUILD_DEPENDS_IPS="developer/build/gnu-make system/header system/library/math omniti/perl/mldbm omniti/perl/mldbm-sync omniti/perl/cgi"
 
 PREFIX=/opt/OMNIperl
 reset_configure_opts
@@ -47,7 +47,7 @@ NO_PARALLEL_MAKE=1
 PERLVERLIST="5.14 5.16 5.20"
 
 # Add any additional deps here; OMNIperl added below
-DEPENDS_IPS="omniti/perl/mldbm omniti/perl/mldbm-sync"
+DEPENDS_IPS="omniti/perl/mldbm omniti/perl/mldbm-sync omniti/perl/cgi"
 
 # We require a Perl version to use for this build and there is no default
 case $DEPVER in
