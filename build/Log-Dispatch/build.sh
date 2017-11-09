a7298f679148a592df46009d6b8afd4451ff671e
mmurphy <mmurphy@omniti.com>
2017-11-09 18:49:30 +0000

    update log-dispatch dependencies

--- build/Log-Dispatch/build.sh
+++ build/Log-Dispatch/build.sh
@@ -36,7 +36,7 @@ PKG=omniti/perl/$(echo $PROG | tr '[A-Z]' '[a-z]')
 SUMMARY="Dispatches messages to one or more outputs (Perl $DEPVER)"
 DESC="$SUMMARY"
 
-BUILD_DEPENDS_IPS="developer/build/gnu-make system/header system/library/math omniti/perl/class-load omniti/perl/params-validate omniti/perl/namespace-autoclean omniti/perl/namespace-clean"
+BUILD_DEPENDS_IPS="developer/build/gnu-make system/header system/library/math omniti/perl/class-load omniti/perl/params-validate omniti/perl/namespace-autoclean omniti/perl/namespace-clean omniti/perl/specio"
 
 PREFIX=/opt/OMNIperl
 reset_configure_opts
@@ -47,7 +47,7 @@ NO_PARALLEL_MAKE=1
 PERLVERLIST="5.14 5.16 5.20"
 
 # Add any additional deps here; omniti/runtime/perl added below
-DEPENDS_IPS="omniti/perl/class-load omniti/perl/params-validate omniti/perl/namespace-autoclean omniti/perl/namespace-clean"
+DEPENDS_IPS="omniti/perl/class-load omniti/perl/params-validate omniti/perl/namespace-autoclean omniti/perl/namespace-clean omniti/perl/specio"
 
 # We require a Perl version to use for this build and there is no default
 case $DEPVER in
